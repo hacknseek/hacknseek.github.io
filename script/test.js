422ea00f4190bee124098af88bb8b4212366412f
hacknseek <r02943120@ntu.edu.tw>
2018-09-16 22:26:08 +0800

    add a random number generator

--- script/test.js
+++ script/test.js
@@ -1,1 +1,11 @@
 console.log('hi');
+
+
+function button(){
+    var x = document.getElementById("input").value;
+    document.getElementById("response").innerHTML = x;
+    if (!isNaN(x)){
+        console.log('x');
+        document.getElementById("response").innerHTML = Math.floor(Math.random() * x);
+    }
+}
